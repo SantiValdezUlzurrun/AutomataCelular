0eddff20a049a5c65652d69d104d92a08d079f92
Santiago Valdez <santiagoval1999@gmail.com>
2021-03-01 01:08:38 -0300

    feat: interfaz grafica para regla 110

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,17 @@
 module Main where
 
-import System.Random
+import Graphics.Gloss
+import Graphics.Gloss.Data.Color
+
 import Regla110
 
-iter :: Int
-iter = 20
 tam :: Int
 tam = 100
 
-genInicial :: Int -> IO [Int]
-genInicial n = sequence $ replicate n $ randomRIO (0, 1)
+vEntAnAs :: Display
+vEntAnAs = InWindow "Regla110" (640, 480) (100, 100)
+
+colorDelFondo = makeColor 0 0 0 255
 
 main :: IO ()
-main = do
-	inicial <- genInicial tam 
-	imprimirUniverso iter inicial
+main = simulate vEntAnAs colorDelFondo 1 (genInicial tam) mostrarModelo avanzarGen
--- src/Regla110.hs
+++ src/Regla110.hs
@@ -1,24 +1,66 @@
 module Regla110 where
 
-imprimirUniverso :: Int -> [Int] -> IO ()
-imprimirUniverso n generacion =
-	if n == 0
-		then return ()
-	else do
-		imprimirFila generacion
-		imprimirUniverso (n-1) nuevaGeneracion
-		where
-			nuevaGeneracion = avanzarGen generacion
+import Graphics.Gloss
+import Graphics.Gloss.Data.Picture
+import Graphics.Gloss.Data.ViewPort
 
-imprimirFila :: [Int] -> IO ()
-imprimirFila fila = putStrLn cadena
+import System.Random
+
+data Celula = Viva | Muerta deriving (Eq, Show)
+type Tablero = [[Celula]]
+
+genInicial :: Int -> Tablero
+genInicial n = [map f lista]
+	where 
+		f = (\x -> if x == 1 then Viva else Muerta)
+		lista = [1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1]
+
+
+mostrarModelo :: Tablero -> Picture
+mostrarModelo tablero = pictures $ map pictures imagenesEnPos
 	where
-		cadena = "| " ++ (map (\x -> if x == 1 then '*' else ' ') fila) ++ " |"
+		cantCol = length tablero
+		filasConPosRelY = zip tablero (take cantCol [1..cantCol])
+		imagenesEnPos = map mostrarFila filasConPosRelY
+
+mostrarFila :: ([Celula], Int) -> [Picture]
+mostrarFila (celulas, posRelY) = map trasladar imagenesConPosRel
+	where
+		imagenes = map imagenCelula celulas
+		cantCel = length celulas
+		posRel = zip (take cantCel [1..cantCel]) (take cantCel (repeat posRelY))
+		imagenesConPosRel = zip imagenes posRel
+
+
+trasladar :: (Picture, (Int, Int)) -> Picture
+trasladar (imagen, (posRelX, posRelY)) = translate x y imagen 
+	where
+		x = 10.0 * fromIntegral posRelX
+		y = -10.0 * fromIntegral posRelY
+
+imagenCelula :: Celula -> Picture
+imagenCelula cel = if cel == Viva 
+					then imagenCelulaViva
+				   else imagenCelulaMuerta
+
+colorCelulaViva :: Color
+colorCelulaViva = makeColorI 255 50 50 255
+
+imagenCelulaViva :: Picture
+imagenCelulaViva = pictures [color colorCelulaViva $ rectangleSolid 10 10]
+
+colorCelulaMuerta :: Color
+colorCelulaMuerta = makeColorI 0 0 255 255
+
+imagenCelulaMuerta :: Picture
+imagenCelulaMuerta = pictures [color colorCelulaMuerta $ rectangleSolid 10 10]
+
 
 -- Avanza de generacion segun la regla 110
-avanzarGen :: [Int] -> [Int]
-avanzarGen gen = map' remplazarRegla gen'
-	where 
+avanzarGen :: ViewPort -> Float -> Tablero -> Tablero
+avanzarGen _ _ tablero = tablero ++ [map' remplazarRegla gen']
+	where
+		gen = last tablero
 		primero = head gen
 		ultimo = last gen
 		gen' = ultimo : gen ++ [primero]
@@ -33,13 +75,14 @@ map' _ (x:xs) = []
 111 	110 	101 	100 	011 	010 	001 	000
  0	   	 1 		 1 		 0 		 1 		 1 		 1 		 0 
 --} 
-remplazarRegla :: Int -> Int -> Int -> Int
-remplazarRegla 0 0 0 = 0
-remplazarRegla 0 0 1 = 1
-remplazarRegla 0 1 0 = 1
-remplazarRegla 0 1 1 = 1
-remplazarRegla 1 0 0 = 0
-remplazarRegla 1 0 1 = 1
-remplazarRegla 1 1 0 = 1
-remplazarRegla 1 1 1 = 0
+remplazarRegla :: Celula -> Celula -> Celula -> Celula
+remplazarRegla Muerta Muerta Muerta = Muerta
+remplazarRegla Muerta Muerta Viva = Viva
+remplazarRegla Muerta Viva Muerta = Viva
+remplazarRegla Muerta Viva Viva = Viva
+remplazarRegla Viva Muerta Muerta = Muerta
+remplazarRegla Viva Muerta Viva = Viva
+remplazarRegla Viva Viva Muerta = Viva
+remplazarRegla Viva Viva Viva = Muerta
+
 
